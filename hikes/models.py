a394e89ba942ab2ddfd3dd172a62529202589e9b
StychukMaria <stychuk.maria@gmail.com>
2026-07-13 11:29:09 +0300

    add DifficultyLevel model

--- hikes/models.py
+++ hikes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class DifficultyLevel(models.Model):
+    name = models.CharField(max_length=63, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
