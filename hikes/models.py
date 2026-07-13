6eb156936dd1997f7a8baee4b79f3e1ffc08f921
StychukMaria <stychuk.maria@gmail.com>
2026-07-13 11:35:47 +0300

    add Region model

--- hikes/models.py
+++ hikes/models.py
@@ -9,3 +9,14 @@ class DifficultyLevel(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Region(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    country = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name} ({self.country})"
