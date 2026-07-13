dc6bde6f6faef7a97666d5e14f0d255612838ee0
StychukMaria <stychuk.maria@gmail.com>
2026-07-13 12:18:51 +0300

    add Hiker model

--- hikes/models.py
+++ hikes/models.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.urls import reverse
 
 
 class DifficultyLevel(models.Model):
@@ -20,3 +22,29 @@ class Region(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.country})"
+
+
+class Hiker(AbstractUser):
+    FITNESS_CHOICES = (
+        ('beginner', 'Beginner'),
+        ('intermediate', 'Intermediate'),
+        ('advanced', 'Advanced'),
+        ('expert', 'Expert'),
+    )
+
+    fitness_level = models.CharField(
+        max_length=20,
+        choices=FITNESS_CHOICES,
+        default='beginner'
+    )
+    has_tent = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = "Hiker"
+        verbose_name_plural = "Hikers"
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+    def get_absolute_url(self):
+        return reverse("hikes:hiker-detail", kwargs={"pk": self.pk})
