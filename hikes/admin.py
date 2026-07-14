379a6f5d4fa5cb31f44a12e0fa3e1ccbef69d6cc
StychukMaria <stychuk.maria@gmail.com>
2026-07-14 17:13:47 +0300

    register Hiker, Region, DifficultyLevel, and Expedition in admin

--- hikes/admin.py
+++ hikes/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from hikes.models import Hiker, DifficultyLevel, Region, Expedition
+
+
+@admin.register(Hiker)
+class HikerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("fitness_level", "has_tent")
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("fitness_level", "has_tent")}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Additional info",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "fitness_level",
+                    "has_tent",
+                )
+            },
+        ),
+    )
+
+
+admin.site.register(DifficultyLevel)
+admin.site.register(Region)
+admin.site.register(Expedition)
