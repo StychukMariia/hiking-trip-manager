bd8013753caf95fb5827ac15e052d821b16dbc3f
StychukMaria <stychuk.maria@gmail.com>
2026-07-18 10:55:44 +0300

    set up index page routing and basic view

--- hikes/views.py
+++ hikes/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+from hikes.models import (
+    Hiker,
+    Expedition,
+    Region
+)
+
+
+def index(request):
+    num_hikers = Hiker.objects.count()
+    num_expeditions = Expedition.objects.count()
+    num_regions = Region.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_hikers": num_hikers,
+        "num_expeditions": num_expeditions,
+        "num_regions": num_regions,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "hikes/index.html", context=context)
